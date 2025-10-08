43945f9bb2b15f399f67e4096d5e71f805e7b4da
Sujeet0101 <suj337raut@gmail.com>
2025-10-08 13:23:01 +0545

    update header, navbar and login

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,6 +1,8 @@
 import {assets} from "../assets/assets"
+import { useNavigate } from "react-router-dom";
 
 const Navbar = () => {
+    const navigate = useNavigate();
   return (
     <div
       className="w-full flex justify-between items-center p-4 sm:p-6
@@ -8,9 +10,12 @@ const Navbar = () => {
     >
       <img src={assets.logo} alt="logo" className="w-28 sm:w-32" />
 
-      <button className="flex items-center gap-2 border border-gray-500
+      <button
+        onClick={() => navigate("/login")}
+        className="flex items-center gap-2 border border-gray-500
       rounded-full px-6 py-2 text-gray-800 hover:bg-gray-100
-      transition-all">
+      transition-all"
+      >
         Login <img src={assets.arrow_icon} alt="" />
       </button>
     </div>
--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,11 +1,112 @@
-import React from 'react'
+import {useState} from 'react'
+import { assets } from '../assets/assets';
+import { useNavigate } from 'react-router-dom';
 
 const Login = () => {
+
+  const navigate = useNavigate();
+
+  const [state, setState] = useState('Sign Up');
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
   return (
-    <div>
-        <h1>Login Page</h1>
+    <div
+      className="flex items-center justify-center min-h-screen px-6 sm:px-0 
+    bg-gradient-to-br from-blue-200 to-purple-200"
+    >
+      <img
+        src={assets.logo}
+        alt=""
+        onClick={() => navigate('/')}
+        className="absolute left-5 sm:left-5 top-5 w-28 sm:w-32 cursor-pointer"
+      />
+      <div className="bg-slate-900 p-10 rounded-lg shadow-lg w-full sm:w-96 text-indigo-300 text-sm">
+        <h2 className="text-3xl font-semibold text-white text-center mb-3">
+          {state === "Sign Up" ? "Creat Account" : "Login"}
+        </h2>
+        <p className="text-center text-sm mb-6">
+          {state === "Sign Up"
+            ? "Create your account"
+            : "Login to your account!"}
+        </p>
+
+        <form>
+          {state === "Sign Up" && (
+            <div className="mb-4 flex items-center gap-3 w-full px-5 py-2.5 rounded-full bg-[#333a5c]">
+              <img src={assets.person_icon} alt="" />
+              <input
+                className="bg-transparent outline-none"
+                type="text"
+                placeholder="Full Name"
+                onChange={(e) => setName(e.target.value)}
+                value={name}
+                required
+              />
+            </div>
+          )}
+
+          <div className="mb-4 flex items-center gap-3 w-full px-5 py-2.5 rounded-full bg-[#333a5c]">
+            <img src={assets.mail_icon} alt="" />
+            <input
+              className="bg-transparent outline-none"
+              type="email"
+              placeholder="Email"
+              onChange={(e) => setEmail(e.target.value)}
+              value={email}
+              required
+            />
+          </div>
+          <div className="mb-4 flex items-center gap-3 w-full px-5 py-2.5 rounded-full bg-[#333a5c]">
+            <img src={assets.lock_icon} alt="" />
+            <input
+              className="bg-transparent outline-none"
+              type="password"
+              placeholder="Password"
+              onChange={(e) => setPassword(e.target.value)}
+              value={password}
+              required
+            />
+          </div>
+
+          <p 
+          onClick={() => navigate('/reset-password')}
+          className="mb-4 text-indigo-500 cursor-pointer hover:underline">
+            Forgot password?
+          </p>
+
+          <button
+            className="w-full py-2.5 rounded-full bg-gradient-to-r from-indigo-500 to-indigo-900 
+          text-white font-medium"
+          >
+            {state === "Sign Up" ? "Sign Up" : "Login"}
+          </button>
+        </form>
+        {state === "Sign Up" ? (
+          <p className="text-gray-400 text-center text-xs mt-4">
+            Already have an account?{" "}
+            <span
+              onClick={() => setState("Login")}
+              className="text-blue-400 cursor-pointer underline"
+            >
+              Login here
+            </span>
+          </p>
+        ) : (
+          <p className="text-gray-400 text-center text-xs mt-4">
+            Don't have an account?{" "}
+            <span
+              onClick={() => setState("Sign Up")}
+              className="text-blue-400 cursor-pointer underline"
+            >
+              Sign up
+            </span>
+          </p>
+        )}
+      </div>
     </div>
-  )
+  );
 }
 
 export default Login
